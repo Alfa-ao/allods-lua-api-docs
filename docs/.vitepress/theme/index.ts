04e9cd6584371906b8a1c1e630cb40099a060022
Alfa-ao <294923229+Alfa-ao@users.noreply.github.com>
2026-08-14 12:22:19 +0300

    SidebarAutoScroll

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,13 @@
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
-import './custom.css'
+import SidebarAutoScroll from './SidebarAutoScroll.vue'
+import '/.vitepress/theme/custom.css'
 
-export default DefaultTheme
+export default {
+    extends: DefaultTheme,
+
+    Layout: () =>
+        h(DefaultTheme.Layout, null, {
+            'layout-bottom': () => h(SidebarAutoScroll)
+        })
+}
