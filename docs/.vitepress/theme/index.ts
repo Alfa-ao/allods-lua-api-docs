ea8ff54a6d853dd8ee33e17f0cc3dc3b5ecfadda
Alfa-ao <godenergysoulworker@gmail.com>
2026-09-13 12:19:50 +0300

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,13 +1,13 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import SidebarAutoScroll from './SidebarAutoScroll.vue'
-import '/.vitepress/theme/custom.css'
+import './custom.css'
+
 
 export default {
     extends: DefaultTheme,
-
-    Layout: () =>
-        h( DefaultTheme.Layout, null, {
-            'layout-bottom': () => h( SidebarAutoScroll )
-        } )
-}
+    Layout: () => h( DefaultTheme.Layout, null, {
+        'layout-bottom': () => h( SidebarAutoScroll )
+    } ),
+    
+};
